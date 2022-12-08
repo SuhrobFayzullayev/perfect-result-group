eda04a018c851f15c5cfd8391bbd420eef3979a6
Xurshid4744 <nematoff4744@gmail.com>
2022-12-08 16:46:18 +0500

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./index.scss"
+const Footer = () => {
+  return (
+    <h3>Footer</h3>
+  )
+}
+
+export default Footer
---
+++ src/routes.js
@@ -0,0 +1,6 @@
+import Home from "./pages/Home/Home";
+
+const routes = [
+    { path: `/`, element: <Home /> }
+];
+export default routes;
