58bcdbb4084ed03dd4081a6f5b3a4e579aa458a3
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2022-12-09 04:01:43 -0800

    header and footer are ready

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import Layout from "./components/Layout/Layout"
+import Layout from "./components/Layout/Layout";
 import routes from "./routes";
 
 function App() {
