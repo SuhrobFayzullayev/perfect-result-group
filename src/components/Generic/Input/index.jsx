45d9b3e9eed9e415446cdaa4b47aca8160dfd1c8
https://github.com/Uraimoff <uraimovibrohim5@gmail.com>
2022-12-09 10:42:15 +0500

    antd

---
+++ src/components/Generic/Input/index.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import "./index.scss"
+
+const Inputs = ({
+    width,
+    height,
+    border,
+    placeholder,
+    onChange,
+    password,
+    name,
+    icon,
+    defaultValue,
+    background
+  }) => {
+    const [view, setView] = useState(false);
+  return (
+    <div className='inputWrapper' style={{
+        width: width ? `${width}px` : `100%`,
+      }}>
+        <img className='inputImg' src={icon} alt="" />
+        <div className="input">
+          <div className="placemove">{placeholder}</div>
+          <input className='inputt' type={view ? "text" : password}
+            icon={icon}
+            border={border}
+            placeholder={placeholder}
+            onChange={onChange}
+            defaultValue={defaultValue}
+            name={name}
+            style={{
+              width: width ? `${width}px` : `100%`,
+              height: height ? `${height}px` : `44px`,
+              paddingLeft: icon ? `30px` : "15px",
+              border: border ? `none` : `2px solid #79787A`,
+              backgroundColor: background ? `${background}` : 'transparent'
+            }} />
+          </div>
+    </div>
+  )
+}
+
+export default Inputs
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Name from '../../components/ComponentName'
+import Contact from '../../components/Contacts'
 
 const Home = () => {
   return (
     <>
     <Name name={"hello"}/>
     <div>Home Ibrohim</div>
+    <Contact/>
     </>
   )
 }
