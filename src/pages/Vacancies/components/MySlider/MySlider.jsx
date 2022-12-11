90dccc79bf3b3c2b43330e9c67dcb2ffeb44db69
elshod <elshodtukhtamurodov13@gmail.com>
2022-12-11 16:30:09 +0500

    fixed

--- src/pages/Vacancies/components/MySlider/MySlider.jsx
+++ src/pages/Vacancies/components/MySlider/MySlider.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import "./MySlider.scss";
+
+import { Navigation, A11y, EffectCoverflow } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/effect-coverflow";
 function MySlider() {
   const data = [
     {
@@ -41,44 +48,73 @@ function MySlider() {
   return (
     <div className="slider__wapper">
       <div className="slider">
-        {data.length > 0 &&
-          data.map((item) => {
-            return (
-              <div key={item.id} className="card">
-                <div className="card__title">{item.name}</div>
-                <div className="card__info">
-                  <strong>Office:</strong> <span>{item.office}</span>
-                </div>
-                <div className="card__info">
-                  <strong>Technology:</strong> <span>{item.technology}</span>
-                </div>
-                <div className="card__info">
-                  <strong>TechnologyTelegram:</strong>{" "}
-                  <span>{item.telegram}</span>
-                </div>
-                <div className="card__info">
-                  <strong>Area:</strong> <span>{item.area}</span>
-                </div>
-                <div className="card__info">
-                  <strong>Application time:</strong>{" "}
-                  <span>{item.applicationTime}</span>
-                </div>
-                <div className="card__info">
-                  <strong>Working time:</strong>
-                  <span>
-                    {item.workingTime.from} - {item.workingTime.to}{" "}
-                    {item.workingTime.days}
-                  </span>
-                </div>
-                <div className="card__info">
-                  <strong>Salary:</strong>
-                  <span>
-                    {item.salary.from} - {item.salary.to}
-                  </span>
-                </div>
-              </div>
-            );
-          })}
+        <Swiper
+          navigation
+          pagination={{
+            clickable: true,
+          }}
+          breakpoints={{
+            400: {
+              slidesPerView: 1,
+              spaceBetween: 30,
+              centeredSlides: true,
+            },
+            920: {
+              slidesPerView: 2,
+              spaceBetween: 30,
+              centeredSlides: true,
+            },
+            1340: {
+              slidesPerView: 3,
+              spaceBetween: 30,
+              centeredSlides: true,
+            },
+          }}
+          modules={[Navigation]}
+          className="mySwiper"
+        >
+          {data.length > 0 &&
+            data.map((item) => {
+              return (
+                <SwiperSlide key={item.id}>
+                  <div className="card">
+                    <div className="card__title">{item.name}</div>
+                    <div className="card__info">
+                      <strong>Office: </strong> <span>{item.office}</span>
+                    </div>
+                    <div className="card__info">
+                      <strong>Technology: </strong>
+                      <span>{item.technology.join(", ")}</span>
+                    </div>
+                    <div className="card__info">
+                      <strong>Telegram: </strong>
+                      <span>{item.telegram}</span>
+                    </div>
+                    <div className="card__info">
+                      <strong>Area: </strong> <span>{item.area}</span>
+                    </div>
+                    <div className="card__info">
+                      <strong>Application time: </strong>
+                      <span>{item.applicationTime}</span>
+                    </div>
+                    <div className="card__info">
+                      <strong>Working time: </strong>
+                      <span>
+                        {item.workingTime.from} - {item.workingTime.to}{" "}
+                        {item.workingTime.days}
+                      </span>
+                    </div>
+                    <div className="card__info">
+                      <strong>Salary: </strong>
+                      <span>
+                        {item.salary.from} - {item.salary.to}
+                      </span>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              );
+            })}
+        </Swiper>
       </div>
     </div>
   );
