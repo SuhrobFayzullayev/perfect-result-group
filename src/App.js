1a7b2db16277d31d600f7e83f043e5daf6004020
Пользователь <suhrob.coder0604@gmail.com>
2023-02-14 14:01:40 +0300

    fff

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
-import routes from "./routes";
+import About from "./pages/About";
+import Home from "./pages/Home/Home";
+import PortfolioPage from "./pages/Portfolio/PortfolioPage";
+import Services from "./pages/Services/Services";
+import Vacancies from "./pages/Vacancies/Vacancies";
 
 function App() {
-  
+  const routes = [
+    { path: `/`, element: <Home /> },
+    { path: `/portfolio`, element: <PortfolioPage /> },
+    { path: `/about`, element: <About /> },
+    { path: `/vacancies`, element: <Vacancies /> },
+    { path: `/services`, element: <Services /> },
+  ];
   return (
     <>
-    <Layout>
-      <Routes>
-        {routes.map(({ path, element }) => (
-          <Route path={path} element={element} key={path} />
-        ))}
-      </Routes>
-    </Layout>
+      <Layout>
+        <Routes>
+          {routes.map(({ path, element }) => (
+            <Route path={path} element={element} key={path} />
+          ))}
+        </Routes>
+      </Layout>
     </>
   );
 }
